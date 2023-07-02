aa762fd6628040adf00d7efd72454ea9445058cf
Rashonda Edwards <rashondaedwards@aol.com>
2023-07-02 14:16:05 -0700

    working API

--- src/App.js
+++ src/App.js
@@ -2,33 +2,32 @@ import React, { useState, useEffect } from "react";
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
 import MovieList from "./Components/MovieList";
+import MovieListHeading from "./Components/MovieListHeading";
+import SearchBox from "./Components/SearchBox";
 
 function App() {
-    const [movies, setMovies] = useState([
-        {
-            Title: "Star Wars: Episode IV - A New Hope",
-            Year: "1977",
-            imdbID: "tt0076759",
-            Type: "movie",
-            Poster: "https://m.media-amazon.com/images/M/MV5BOTA5NjhiOTAtZWM0ZC00MWNhLThiMzEtZDFkOTk2OTU1ZDJkXkEyXkFqcGdeQXVyMTA4NDI1NTQx._V1_SX300.jpg",
-        },
-        {
-            Title: "Star Wars: Episode V - The Empire Strikes Back",
-            Year: "1980",
-            imdbID: "tt0080684",
-            Type: "movie",
-            Poster: "https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
-        },
-        {
-            Title: "Star Wars: Episode VI - Return of the Jedi",
-            Year: "1983",
-            imdbID: "tt0086190",
-            Type: "movie",
-            Poster: "https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg",
-        },
-    ]);
+    const [movies, setMovies] = useState([]);
+    const [searchValue, setSearchValue] = useState('')
+
+    const getMovieRequest = async(searchValue)=>{
+        const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=9d06303`
+        const response = await fetch(url)
+        const responseJSON = await response.json()
+        if (responseJSON.Search) {
+            setMovies(responseJSON.Search)
+        }
+    }
+
+    useEffect(()=>{
+        getMovieRequest(searchValue)
+    }, [searchValue])
+
     return (
         <div className="container-fluid movie-app">
+            <div className="row d-flex align-items-center mt-4 mb-4">
+                <MovieListHeading heading='Movies'/>
+                <SearchBox searchValue={searchValue} setSearchValue={setSearchValue}/>
+            </div>
             <div className="row">
                 <MovieList movies={movies} />
             </div>
