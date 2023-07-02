75c468b90f449faca58d099b4233b3bcf214b77f
Brenda <amaya.brendaa@gmail.com>
2023-07-02 09:47:42 -0700

    testing -b

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/navbar';
 
 function App() {
+
+  document.title = 'MoviesRUs'
   return (
     <div className="App">
-
+      <NavBar />
     </div>
   );
 }
