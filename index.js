a8919830debc80090ab8d4cc69d6afd861434399
Brenda <amaya.brendaa@gmail.com>
2023-07-02 21:40:35 -0700

    error page added

--- index.js
+++ index.js
@@ -2,17 +2,24 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+//  DEFINES THE VIEW ENGINE
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
+//  ROUTES
 app.use('/movies', require('./controllers/movies'))
 app.use('/series', require('./controllers/series'))
 app.use('/videogames', require('./controllers/videogames'))
 app.use('/favorites', require('./controllers/favorites'))
 
+//  HOME PAGE
 app.get('/', (req,res) => {
-    res.send('Hello World!')
+    res.render('home')
 })
 
+//  404 PAGE
 app.get('*', (req,res) => {
-    res.status(404).send('<h1>404 Page</h1>')
+    res.render('error404')
 })
 
 app.listen(process.env.PORT)
---
+++ views/home.jsx
@@ -0,0 +1,15 @@
+const React = require('react')
+const Def = require('./default')
+
+
+function home () {
+    return(
+        <Def>
+            <main>
+                HOME
+            </main>
+        </Def>
+    )
+}
+
+module.exports = home
