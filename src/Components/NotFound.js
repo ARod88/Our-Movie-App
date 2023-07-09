8bea2152361767ea64c4235f9ff99d3e01251b51
Brenda <amaya.brendaa@gmail.com>
2023-07-09 12:36:26 -0700

    added photo to 404 page

--- src/Components/NotFound.js
+++ src/Components/NotFound.js
@@ -1,11 +1,14 @@
 import React from "react"
 import { Link } from "react-router-dom"
 
+const movieSlatePicture = new URL('../images/movie-slate-open.png', import.meta.url);
+
 export default function NotFound(){
     return(
         <div>
             <h1>404 ERROR : PAGE NOT FOUND</h1>
-            <p>Oops! You seem to be lost! This page does not exist! </p>
+            <h2 className="notfound">Oops! You seem to be lost! This page does not exist! </h2>
+            <img className="movie-slate" src={movieSlatePicture}/>
         </div>
     )
 }
