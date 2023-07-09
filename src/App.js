0b7133e5776574381fa541e02fa39960bda349f5
Rashonda Edwards <rashondaedwards@aol.com>
2023-07-09 12:26:41 -0700

    more stuf

--- src/App.js
+++ src/App.js
@@ -17,16 +17,15 @@ document.title = "MovieApp";
 
 function App() {
     return (
-            
-                <Router>
-                    <header>
-                        <Container>
-                            <Nav defaultActiveKey="/" variant="tabs" fill>
-                                <Nav.Item>
-                                    <Link to="/">
-                                        <Nav.Link href="/">Home</Nav.Link>
-                                    </Link>
-                                </Nav.Item>
+        <Router>
+            <header>
+                <Container>
+                    <Nav defaultActiveKey="/" variant="tabs" fill>
+                        <Nav.Item>
+                            <Link to="/">
+                                <Nav.Link href="/">Home</Nav.Link>
+                            </Link>
+                        </Nav.Item>
 
                         <Nav.Item>
                             <Link to="/searchpage">
@@ -39,27 +38,19 @@ function App() {
                             </Link>
                         </Nav.Item>
 
-                    
-
-                                
-                             
-
-                                <Nav.Item>
-                                    <Link to="/favorites">
-                                        <Nav.Link
-                                            href="/favorites"
-                                            eventKey={"favoritespage"}
-                                        >
-                                            Favorites
-
-                                        </Nav.Link>
-                                    </Link>
-                                </Nav.Item>
-
-                               
-                            </Nav>
-                        </Container>
-                    </header>
+                        <Nav.Item>
+                            <Link to="/favorites">
+                                <Nav.Link
+                                    href="/favorites"
+                                    eventKey={"favoritespage"}
+                                >
+                                    Favorites
+                                </Nav.Link>
+                            </Link>
+                        </Nav.Item>
+                    </Nav>
+                </Container>
+            </header>
 
             <div>
                 <Routes>
@@ -78,4 +69,3 @@ function App() {
 }
 
 export default App;
-
