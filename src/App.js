a714e520e44fb1bfe6fe48a2863f59b3d82139a6
Rashonda Edwards <rashondaedwards@aol.com>
2023-07-10 13:54:15 -0700

    added movie search placeholders

--- src/App.js
+++ src/App.js
@@ -3,70 +3,20 @@ import React from "react";
 import NavBar from "./Components/NavBar";
 import "./App.css";
 
-import Home from "./Components/Home";
-import Favorites from "./Components/Favorites";
-import NotFound from "./Components/NotFound";
-import SearchPage from "./Components/SearchPage";
-import MovieShowPage from "./Components/MovieShowPage";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
-import Navbar from "react-bootstrap/Navbar";
+
 
 document.title = "MovieApp";
 
+
+
 function App() {
-    return (
-        <Router>
-            <header>
-                <Container>
-                    <Nav defaultActiveKey="/" variant="tabs" fill>
-                        <Nav.Item>
-                            <Link to="/">
-                                <Nav.Link href="/">Home</Nav.Link>
-                            </Link>
-                        </Nav.Item>
-
-                        <Nav.Item>
-                            <Link to="/searchpage">
-                                <Nav.Link
-                                    href="/SearchPage"
-                                    eventKey={"searchpage"}
-                                >
-                                    SearchPage
-                                </Nav.Link>
-                            </Link>
-                        </Nav.Item>
-
-                        <Nav.Item>
-                            <Link to="/favorites">
-                                <Nav.Link
-                                    href="/favorites"
-                                    eventKey={"favoritespage"}
-                                >
-                                    Favorites
-                                </Nav.Link>
-                            </Link>
-                        </Nav.Item>
-                    </Nav>
-                </Container>
-            </header>
-
-            <div>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/favorites" element={<Favorites />} />
-                    <Route path="/searchpage" element={<SearchPage />} />
-                    <Route
-                        path="/movieshowpage/:id"
-                        element={<MovieShowPage />}
-                    />
-                    <Route path="*" element={<NotFound />} />
-                </Routes>
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <NavBar />
+    </Router>
+  );
 }
 
 export default App;
--- src/Components/SearchPage.js
+++ src/Components/SearchPage.js
@@ -10,7 +10,62 @@ const movieSlatePic2 = new URL(
 );
 
 const SearchPage = () => {
-    const [movies, setMovies] = useState([]);
+    const [movies, setMovies] = useState([{
+        "Title": "The Lego Movie",
+        "Year": "2014",
+        "imdbID": "tt1490017",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BMTg4MDk1ODExN15BMl5BanBnXkFtZTgwNzIyNjg3MDE@._V1_SX300.jpg"
+    },
+    {
+        "Title": "The Simpsons Movie",
+        "Year": "2007",
+        "imdbID": "tt0462538",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BNjc4NmQyNGUtMDg4NS00ZTZkLWI3ODQtMGJmYThiYjQxNGRiXkEyXkFqcGdeQXVyMTA0MTM5NjI2._V1_SX300.jpg"
+    },
+    {
+        "Title": "Scary Movie",
+        "Year": "2000",
+        "imdbID": "tt0175142",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BMGEzZjdjMGQtZmYzZC00N2I4LThiY2QtNWY5ZmQ3M2ExZmM4XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
+    },
+    {
+        "Title": "El Camino: A Breaking Bad Movie",
+        "Year": "2019",
+        "imdbID": "tt9243946",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BNjk4MzVlM2UtZGM0ZC00M2M1LThkMWEtZjUyN2U2ZTc0NmM5XkEyXkFqcGdeQXVyOTAzMTc2MjA@._V1_SX300.jpg"
+    },
+    {
+        "Title": "Bee Movie",
+        "Year": "2007",
+        "imdbID": "tt0389790",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BMjE1MDYxOTA4MF5BMl5BanBnXkFtZTcwMDE0MDUzMw@@._V1_SX300.jpg"
+    },
+    {
+        "Title": "Scary Movie 2",
+        "Year": "2001",
+        "imdbID": "tt0257106",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BMzQxYjU1OTUtYjRiOC00NDg2LWI4MWUtZGU5YzdkYTcwNTBlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
+    },
+    {
+        "Title": "The Lego Batman Movie",
+        "Year": "2017",
+        "imdbID": "tt4116284",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BMTcyNTEyOTY0M15BMl5BanBnXkFtZTgwOTAyNzU3MDI@._V1_SX300.jpg"
+    },
+    {
+        "Title": "The Super Mario Bros. Movie",
+        "Year": "2023",
+        "imdbID": "tt6718170",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BOTJhNzlmNzctNTU5Yy00N2YwLThhMjQtZDM0YjEzN2Y0ZjNhXkEyXkFqcGdeQXVyMTEwMTQ4MzU5._V1_SX300.jpg"
+    }]);
     const [searchValue, setSearchValue] = useState("");
     const [searchType, setSearchType] = useState("");
 
@@ -29,8 +84,8 @@ const SearchPage = () => {
 
     return (
         <div className="container-fluid movie-app">
+            <MovieListHeading heading={searchValue} />
           <div className="row d-flex align-items-center mt-2 mb-4 custom-margin">
-
             <SearchBox
               searchValue={searchValue}
               setSearchValue={setSearchValue}
